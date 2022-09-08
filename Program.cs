487c8898e7cac61578d4e54109800fed160484a8
Ilya <krotov891@gmail.com>
2022-09-08 20:23:09 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+int[,] array = new int[10,7];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void Print(int[,] _array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{ array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
